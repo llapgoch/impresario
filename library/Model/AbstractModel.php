13c738d825bdd5cc9c9715cad0173374134d3681
Dave Baker <dave_preece@mac.com>
2018-08-14 19:38:07 +0100

    Base models

---
+++ library/Model/AbstractModel.php
@@ -0,0 +1,22 @@
+<?php
+
+class AbstractModel
+{
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @param $id
+     */
+    public function load($id)
+    {
+
+    }
+
+    public function save()
+    {
+
+    }
+}
---
+++ library/Model/Quote.php
@@ -0,0 +1,6 @@
+<?php
+
+class Quote extends AbstractModel
+{
+    
+}
