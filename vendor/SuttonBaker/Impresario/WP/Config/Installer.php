fd1aec6d5238f9d65c3141b3fa9d6a0725efdd27
Dave Baker <dave_preece@mac.com>
2018-08-16 18:01:11 +0100

    Reset version

--- vendor/SuttonBaker/Impresario/WP/Config/Installer.php
+++ vendor/SuttonBaker/Impresario/WP/Config/Installer.php
@@ -5,6 +5,6 @@ namespace SuttonBaker\Impresario\WP\Config;
 class Installer extends \DaveBaker\Core\WP\Config\Installer
 {
     protected $config = [
-        'version' => '0.1.0'
+        'version' => '0.0.1'
     ];
 }
