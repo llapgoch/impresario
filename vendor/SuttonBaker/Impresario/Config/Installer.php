6bf78c8d589bde7fa5da7d976d1e770e24e9e731
Dave Baker <dave_preece@mac.com>
2018-09-04 15:06:29 +0100

    Nudged installers

--- vendor/SuttonBaker/Impresario/Config/Installer.php
+++ vendor/SuttonBaker/Impresario/Config/Installer.php
@@ -13,10 +13,10 @@ class Installer extends \DaveBaker\Core\Config\Installer
     public function __construct()
     {
         $this->mergeConfig([
-            'impresario_client' => '0.0.4',
-            'impresario_enquiry' => '0.0.8',
-            'impresario_task' => '0.0.4',
-            'impresario_quote' => '0.0.2'
+            'impresario_client' => '0.0.5',
+            'impresario_enquiry' => '0.0.9',
+            'impresario_task' => '0.0.5',
+            'impresario_quote' => '0.0.3'
         ]);
     }
 }
