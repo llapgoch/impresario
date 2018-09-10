ad5453aeaf2ee636f16f91174313efc42117cfe2
Dave Baker <dave_preece@mac.com>
2018-09-10 20:24:13 +0100

    Client replacer table block

--- vendor/SuttonBaker/Impresario/Api/Client.php
+++ vendor/SuttonBaker/Impresario/Api/Client.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace SuttonBaker\Impresario\Api;
-use SuttonBaker\Impresario\Definition\Roles;
+use \SuttonBaker\Impresario\Definition\Client as ClientDefinition;
+use DaveBaker\Core\Definitions\Table as TableDefinition;
 
 /**
  * Class Client
@@ -11,21 +12,43 @@ use SuttonBaker\Impresario\Definition\Roles;
 class Client
     extends \DaveBaker\Core\Api\Base
 {
+
     /**
      * @param $params
-     * @param $request
-     * @return array
+     * @param \WP_REST_Request $request
      * @throws \DaveBaker\Core\App\Exception
+     * @throws \DaveBaker\Core\Block\Exception
      * @throws \DaveBaker\Core\Object\Exception
      */
-    public function updatetableAction($params, $request)
+    public function updatetableAction($params, \WP_REST_Request $request)
     {
-        $tag = $this->getApp()->getBlockManager()->createBlock(
-            '\DaveBaker\Core\Block\Html\Tag',
-            'enquiry.edit.form.errors'
-        )->setTagText("WOOOOO");
+        /** @var \SuttonBaker\Impresario\Helper\Client $clientHelper */
+        $clientHelper = $this->createAppObject('\SuttonBaker\Impresario\Helper\Client');
+        /** @var \SuttonBaker\Impresario\Model\Db\Client\Collection $instanceItems */
+        $instanceItems = $clientHelper->getClientCollection();
+        $tableHeaders = ClientDefinition::TABLE_HEADERS;
 
-        $this->addReplacerBlock($tag);
+
+        /** @var \SuttonBaker\Impresario\Block\Table\StatusLink $tableBlock */
+        $tableBlock = $this->getApp()->getBlockManager()->createBlock(
+            '\SuttonBaker\Impresario\Block\Table\StatusLink',
+            'client.list.table'
+        )->setHeaders($tableHeaders)->setRecords($instanceItems)->addEscapeExcludes(
+            ['delete_column']
+        )->addClass('table-striped js-table-updater')
+            ->addSortableColumns(ClientDefinition::SORTABLE_COLUMNS)
+            ->addAttribute([
+                TableDefinition::ELEMENT_DATA_KEY_TABLE_UPDATER_ENDPOINT =>
+                    $this->getUrlHelper()->getApiUrl(ClientDefinition::API_ENDPOINT_UPDATE_TABLE)
+            ]);
+
+
+        if(isset($params['order']['dir']) && isset($params['order']['column'])){
+            $tableBlock->setColumnOrder($params['order']['column'], $params['order']['dir']);
+        }
+
+
+        $this->addReplacerBlock($tableBlock);
     }
 
 }
