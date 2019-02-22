66ca37b8ca6e96299b78f876d4cb8518259c45bd
Dave Baker <dave_bakes@icloud.com>
2019-02-22 18:38:25 +0000

    Added notes

--- vendor/SuttonBaker/Impresario/Definition/Project.php
+++ vendor/SuttonBaker/Impresario/Definition/Project.php
@@ -71,6 +71,7 @@ class Project
         'actual_cost' => 'Actual Cost',
         'actual_profit' => 'Actual Profit',
         'actual_margin' => 'Actual Margin',
+        'notes' => 'Notes',
         'status' => 'Status'
     ];
 
