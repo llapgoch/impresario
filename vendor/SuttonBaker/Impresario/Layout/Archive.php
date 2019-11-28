f371432a083793fd57864beb5320eb1f7f17d592
Dave Baker <dave_bakes@icloud.com>
2019-11-28 23:53:05 +0000

    Added filters to archive

--- vendor/SuttonBaker/Impresario/Layout/Archive.php
+++ vendor/SuttonBaker/Impresario/Layout/Archive.php
@@ -4,6 +4,7 @@ namespace SuttonBaker\Impresario\Layout;
 
 use \SuttonBaker\Impresario\Definition\Project as ProjectDefinition;
 use \SuttonBaker\Impresario\Definition\Archive as ArchiveDefinition;
+
 /**
  * Class Client
  * @package SuttonBaker\Impresario\Layout
@@ -20,10 +21,11 @@ class Archive extends Base
         $this->addHeading()->addMessages();
 
         $this->addBlock(
-        /** @var \SuttonBaker\Impresario\Block\Core\Tile\Black $mainTile */
+            /** @var \SuttonBaker\Impresario\Block\Core\Tile\Black $mainTile */
             $mainTile = $this->createBlock(
                 '\SuttonBaker\Impresario\Block\Core\Tile\Black',
-                "project.tile.main")
+                "project.tile.main"
+            )
                 ->setHeading("<strong>Archived</strong> Projects")
                 ->setShortcode('body_content')
                 ->setTileBodyClass('nopadding table-responsive')
@@ -72,5 +74,75 @@ class Archive extends Base
                 ->setSortableColumns(ArchiveDefinition::SORTABLE_COLUMNS)
                 ->setEndpointUrl(ArchiveDefinition::API_ENDPOINT_UPDATE_TABLE)
         );
+        
+        $this->createFilterSet($mainTile);
+        return $this;
+    }
+
+    /**
+     * @param string $location
+     * @return $this
+     */
+    public function createFilterSet(
+        $location
+    ) {
+        /** @var \SuttonBaker\Impresario\Block\Form\Filter\Set $filterSet */
+        $location->addChildBlock(
+            $filterSet = $location->createBlock(
+                \SuttonBaker\Impresario\Block\Form\Filter\Set::class,
+                "project.filter.set",
+                'controls'
+            )->setCapabilities($this->getEnquiryHelper()->getViewCapabilities())
+                ->setSetName('project_filters')
+                ->addClass('js-project-filters')
+                ->addJsDataItems([
+                    'tableUpdaterSelector' => '.js-project-table'
+                ])
+        );
+
+        // Clients
+        $clients = $this->createCollectionSelectConnector()
+            ->configure(
+                $this->getClientHelper()->getClientCollection(),
+                'client_id',
+                'client_name'
+            )->getElementData();
+
+        $filterSet->addFilter(
+            $filterSet->createBlock(\SuttonBaker\Impresario\Block\Form\Filter\Select::class)
+                ->setLabelName('Client')
+                ->setFormName('client_id')
+                ->setSelectOptions($clients)
+        );
+
+        $filterSet->addFilter(
+            $filterSet->createBlock(\SuttonBaker\Impresario\Block\Form\Filter\Text::class)
+                ->setLabelName('Site')
+                ->setFormName('site_name')
+        );
+
+        $filterSet->addFilter(
+            $filterSet->createBlock(\SuttonBaker\Impresario\Block\Form\Filter\Text::class)
+                ->setLabelName('Project')
+                ->setFormName('project_name')
+        );
+
+        if ($projectManagers = $this->getRoleHelper()->getProjectManagers()) {
+            $projectManagers = $this->createCollectionSelectConnector()
+                ->configure(
+                    $projectManagers,
+                    'ID',
+                    'display_name'
+                )->getElementData();
+
+            $filterSet->addFilter(
+                $filterSet->createBlock(\SuttonBaker\Impresario\Block\Form\Filter\Select::class)
+                    ->setLabelName('Contracts Manager')
+                    ->setFormName('project_manager_id')
+                    ->setSelectOptions($projectManagers)
+            );
+        }
+
+        return $this;
     }
 }
