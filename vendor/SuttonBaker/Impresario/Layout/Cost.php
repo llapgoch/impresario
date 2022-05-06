8825e4e891e84639ca708f2390b7c2ddda71b215
Dave Baker <dave@awaredigital.co.uk>
2022-05-06 16:54:20 +0100

    Cost change

--- vendor/SuttonBaker/Impresario/Layout/Cost.php
+++ vendor/SuttonBaker/Impresario/Layout/Cost.php
@@ -3,6 +3,9 @@
 namespace SuttonBaker\Impresario\Layout;
 
 use  \SuttonBaker\Impresario\Definition\Cost as CostDefinition;
+use SuttonBaker\Impresario\Definition\Page;
+use  \SuttonBaker\Impresario\Definition\Project as ProjectDefinition;
+
 /**
  * Class Cost
  * @package SuttonBaker\Impresario\Layout
@@ -31,26 +34,66 @@ class Cost extends Base
         /** @var \SuttonBaker\Impresario\Model\Db\Cost $entityInstance */
         $entityInstance = $this->createAppObject('\SuttonBaker\Impresario\Model\Db\Cost');
 
-        if($entityId = $this->getRequest()->getParam(self::ID_KEY)){
+        if ($entityId = $this->getRequest()->getParam(self::ID_KEY)) {
             $entityInstance->load($entityId);
 
-            if(!$entityInstance->getId()){
+            if (!$entityInstance->getId()) {
                 return;
             }
         }
 
         $this->addHeading()->addMessages();
-        $costTypeName = $this->getCostHelper()->determineCostTypeName($entityInstance);
+
+        if ($entityInstance->getId()) {
+            $costType = $entityInstance->getCostType();
+        } else {
+            $costType = $this->getRequest()->getParam(CostDefinition::COST_TYPE_PARAM);
+        }
 
         $this->addBlock(
-        /** @var \SuttonBaker\Impresario\Block\Core\Tile\Black $mainTile */
+            /** @var \SuttonBaker\Impresario\Block\Core\Tile\Black $mainTile */
             $mainTile = $this->createBlock(
                 '\SuttonBaker\Impresario\Block\Core\Tile\Black',
-                "{$this->getBlockPrefix()}.tile.main")
+                "{$this->getBlockPrefix()}.tile.main"
+            )
                 ->setHeading(
-                    $this->getCostHelper()->getActionVerb($entityInstance) . " <strong>Purchase Order for " . $costTypeName . "</strong>")
+                    $this->getCostHelper()->getActionVerb($entityInstance) . " <strong>Purchase Order for " . $costTypeName . "</strong>"
+                )
                 ->setShortcode('body_content')
         );
+
+        // Only projects are supported at the moment
+        if ($costType === 'project') {
+            if ($entityInstance->getId()) {
+                $parentId = $entityInstance->getParentId();
+            } else {
+                $parentId = $this->getRequest()->getParam(CostDefinition::PARENT_ID_PARAM);
+            }
+
+            if ($parentId) {
+                $projectHref = $this->getUrlHelper()->getPageUrl(
+                    Page::PROJECT_EDIT,
+                    ['project_id' => $parentId]
+                );
+                // Create the tab block for the project link
+                $tabs = [
+                    [
+                        'name' => 'Back To Project',
+                        'href' => $projectHref,
+                        'icon' => ProjectDefinition::ICON
+                    ]
+                ];
+
+                $tabBlock = $this->getApp()->getBlockManager()->createBlock(
+                    \SuttonBaker\Impresario\Block\Core\Tile\Tabs::class,
+                    "cost.tile.tabs",
+                    'tabs'
+                )->setTabs($tabs);
+
+                $mainTile->addChildBlock($tabBlock);
+            }
+        }
+
 
         $mainTile->addChildBlock(
             $this->createBlock(
@@ -60,7 +103,5 @@ class Cost extends Base
             )->setElementName('cost_edit_form')
 
         );
-
     }
-
 }
